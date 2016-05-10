e0c144cd071c18b56a669db562568c5caa34268d
Ricardo <rchiock@level715.com>
2016-05-10 18:37:47 -0500

    Preparando JS de Search

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 $(document).ready( function () {
 
+	$("#SearchButton").on("click",function(a){
+		alert("Hello");
+		//$(".overlay-content").toggleClass("overlay-content-on");
+		a.preventDefault();
 
 });
