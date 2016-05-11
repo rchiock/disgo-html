436b6b47652d5d699c2e4b526f2f991a224f846f
Ricardo <rchiock@level715.com>
2016-05-11 16:57:35 -0500

    Corregido Search screen. Agregado funcionalidad topbar.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,23 @@
-$(document).ready( function () {
+$(document).ready(function(){
 
-	$("#SearchButton").on("click",function(a){
-		alert("Hello");
-		//$(".overlay-content").toggleClass("overlay-content-on");
+	/* Open/Close Search window
+	-------------------------------------------------- */
+	$(".SearchButton").on("click", function(a){
+		//alert("Hello");
+		$(".overlay-content").toggleClass("overlay-content-on");
+		$(".wrapper").toggleClass("main-content-off");
 		a.preventDefault();
+	})
+
+	/* Toggle Display Scroll to Top
+	-------------------------------------------------- */
+	$(window).scroll(function(){
+		var sectionUp = $(window).scrollTop();
+		if (sectionUp>=140) {
+			$(".TopBar").css("display","block");
+		} else {
+			$(".TopBar").css("display","none");
+		}
+	})
 
 });
